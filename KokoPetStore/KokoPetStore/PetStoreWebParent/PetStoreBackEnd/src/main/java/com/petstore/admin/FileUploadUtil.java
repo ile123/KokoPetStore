967081947064cb995e4fec1198846f903d80b0a4
ile <ilario.batistic@gmail.com>
2022-08-30 14:11:07 +0200

    FINAL UPDATE

--- KokoPetStore/KokoPetStore/PetStoreWebParent/PetStoreBackEnd/src/main/java/com/petstore/admin/FileUploadUtil.java
+++ KokoPetStore/KokoPetStore/PetStoreWebParent/PetStoreBackEnd/src/main/java/com/petstore/admin/FileUploadUtil.java
@@ -15,20 +15,20 @@ public class FileUploadUtil {
 	private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadUtil.class);
 	
 	public static void saveFile(String uploadDir, String fileName,
-			MultipartFile multipartFile) throws IOException{
-		Path uploadPath = Paths.get(uploadDir);
-		
-		if(!Files.exists(uploadPath)) {
-			Files.createDirectories(uploadPath);
-		}
-		
-		try (InputStream inputStream = multipartFile.getInputStream()){
-			Path filePath = uploadPath.resolve(fileName);
-			Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
-		} catch(IOException ex) {
-			throw new IOException("Could not save file:" + fileName, ex);
-		}
-	}
+            MultipartFile multipartFile) throws IOException {
+        Path uploadPath = Paths.get(uploadDir);
+         
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+         
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioe) {        
+            throw new IOException("Could not save image file: " + fileName, ioe);
+        }      
+    }
 	
 	public static void cleanDir(String dir) {
 		Path dirPath = Paths.get(dir);
--- KokoPetStore/KokoPetStore/PetStoreWebParent/PetStoreBackEnd/src/main/java/com/petstore/admin/MvcConfig.java
+++ KokoPetStore/KokoPetStore/PetStoreWebParent/PetStoreBackEnd/src/main/java/com/petstore/admin/MvcConfig.java
@@ -1,28 +1,28 @@
 package com.petstore.admin;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.file.*;
 
+@Configuration
 public class MvcConfig implements WebMvcConfigurer {
-
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		String categoryImagesDirName = "../category-images";
-		Path categoryImagesDir = Paths.get(categoryImagesDirName);
-		
-		String categoryImagePath = categoryImagesDir.toFile().getAbsolutePath();
-		
-		registry.addResourceHandler("/category-images/**")
-		.addResourceLocations("file:/" + categoryImagePath + "/");
-		
-		String brandImagesDirName = "../brand-images";
-		Path brandImagesDir = Paths.get(brandImagesDirName);
-		
-		String brandImagePath = brandImagesDir.toFile().getAbsolutePath();
-		
-		registry.addResourceHandler("/brand-images/**")
-		.addResourceLocations("file:/" + brandImagePath + "/");
-	}
+ 
+ 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        exposeDirectory("brand-images", registry);
+        exposeDirectory("category-images", registry);
+        exposeDirectory("product-images", registry);
+    }
+     
+    private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
+        Path uploadDir = Paths.get(dirName);
+        String uploadPath = uploadDir.toFile().getAbsolutePath();
+         
+        if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
+         
+        registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:/"+ uploadPath + "/");
+    }
 }
